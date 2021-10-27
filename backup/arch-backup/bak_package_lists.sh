b8296bfa2b1185be2f62b6fbe4247c32b8e8c7c9
Krystian Safjan <ksafjan@gmail.com>
2021-10-27 22:15:44 +0200

    Add backup scripts

---
+++ backup/arch-backup/bak_package_lists.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bd="$HOME/_backup_packages_lists"
+mkdir -p "$bd"
+pacman -Qqe > $bd/pacman_Qqe.lst
+pacman -Qqm > $bd/pacman_Qqm.lst
+pacman -Qqet > $bd/pacman_Qqet.lst
+# pacman -Qqg base > $bd/pacman_Qqg_base.lst
+pacman -Qm > $bd/pacman_Qm.lst
+
+pacman -Qqe | grep -v "$(pacman -Qqm)" > $bd/pacman_Qqe-Qqm.lst
+# pacman -Qqet | grep -v "$(pacman -Qqg base)" | grep -v "$(pacman -Qqm)" > $bd/pacman_Qqe-Qqg_base-Qqm.lst
+pacman -Qm > $bd/pacman_Qm.lst
+
+pip list > $bd/pip.txt
