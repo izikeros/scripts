52419f0f835b9960ae2ec57cf9ad44082edad81f
Krystian Safjan <ksafjan@gmail.com>
2021-10-29 17:11:42 +0200

    Create non-existing target path

--- restore/155-symlink-polybar-config-and-scripts.sh
+++ restore/155-symlink-polybar-config-and-scripts.sh
@@ -4,6 +4,7 @@
 source ./helper_functions.sh
 
 PTH=".config/polybar"
+mkdir -p "$HOME/$PTH"
 
 symlink_dotfile "$HOME/dotfiles/dotfiles/$PTH/config" "$HOME/$PTH/config"
 symlink_dotfile "$HOME/dotfiles/dotfiles/$PTH/master.conf" "$HOME/$PTH/master.conf"
