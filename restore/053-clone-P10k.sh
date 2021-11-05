76cd2aea15de2daacb449ce43a9be97326306c74
Krystian Safjan <ksafjan@gmail.com>
2021-11-05 21:47:53 +0100

    Reorder scripts

--- restore/053-clone-P10k.sh
+++ restore/053-clone-P10k.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# TODO: Check if this one is used or one from zgen directory
 git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
---
+++ restore/059-run-zgen.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Run zsh that will invoke zgen and install everything
+# zsh
+
+# or run zgen update
+zgen update
