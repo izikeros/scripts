32a8534a6c7715f472e320375fa05577667c6c12
Krystian Safjan <ksafjan@gmail.com>
2021-11-10 14:23:22 +0100

    Print missing yfinance venv if missing

--- runonce-scripts/ron-501-yfinance.sh
+++ runonce-scripts/ron-501-yfinance.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-source ~/.virtualenvs/yfinance/bin/activate
-# run python script here
-echo "> yfinance advanced analysis will go here <"
-deactivate
+set -e
+
+if [[ -d ~/.virtualenvs/yfinance/bin/activate ]]; then
+	source ~/.virtualenvs/yfinance/bin/activate	
+
+	# run python script here
+	echo "> yfinance advanced analysis will go here <"
+	deactivate
+else
+	echo "missing yfinance venv"
+fi
