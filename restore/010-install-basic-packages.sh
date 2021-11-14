e4ed91d46922c8706ed28ae8bc9cf357b7c41e0f
Krystian Safjan <ksafjan@gmail.com>
2021-11-14 18:41:21 +0100

    Add comment

--- restore/010-install-basic-packages.sh
+++ restore/010-install-basic-packages.sh
@@ -2,9 +2,8 @@
 
 #
 # very basic packages that should be installed on any system server or desktop
-#
-
-set -e
+#  to enable further setup of the system.
+# Basic packages list contains typically top-10 most useful packages
 
 ./pkg-from-the-list.sh ./pkg/basic.txt
 
