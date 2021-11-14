ad3b16afdaa5be6e9f451336a39b7902ad2c4f53
Krystian Safjan <ksafjan@gmail.com>
2021-11-14 18:42:12 +0100

    Improved fet distro info

--- restore/get-distro-pkg-install-command.sh
+++ restore/get-distro-pkg-install-command.sh
@@ -3,13 +3,24 @@
 
 # Arch linux ultimate installer
 # https://github.com/helmuthdu/aui/blob/master/sharedfuncs
-SYS=""
 
+# --- Determine operating system ------
+SYS=""
 if [[ -f /etc/issue ]]; then 
     grep -q  "Ubuntu" /etc/issue && SYS="ubuntu"
     grep -q  "Arch\|archlabs" /etc/issue && SYS="arch"
 fi
 
+if [[ $OSTYPE == 'darwin'* ]]; then
+  SYS="macOS"
+fi
+
+if [ "$SYS" == "" ]; then
+    #echo "No known system (Ubuntu|Arch) detected"
+    exit 1
+fi
+
+# --- Use proper package manager install command ----
 if [ "$SYS" == "ubuntu" ]; then
     #echo "-- Ubuntu Linux detected, using apt-get"
     #echo "-- Updating repo list"
@@ -22,13 +33,9 @@ if [ "$SYS" == "arch" ]; then
     CMD="sudo pacman -Sy --noconfirm"
 fi
 
-if [[ $OSTYPE == 'darwin'* ]]; then
+if [[ "$SYS" == "macOS" ]]; then
   CMD="brew install"
 fi
 
-if [ "$SYS" == "" ]; then
-    #echo "No known system (Ubuntu|Arch) detected"
-    exit 1
-fi
 
 echo "$CMD"
