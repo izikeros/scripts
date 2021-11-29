42eafd05d6498f32227648ef8a89fdcf13c274a1
Krystian Safjan <ksafjan@gmail.com>
2021-11-29 21:01:17 +0100

    Added script for setting git repo user

---
+++ my_scripts/git-ey-user.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# author: Krystian Safjan (ksafjan@gmail.com)
+# Licence MIT
+
+set -e
+
+git config user.email "krystian.safjan@gds.ey.com"
+git config user.name "Krystian Safjan"
+
