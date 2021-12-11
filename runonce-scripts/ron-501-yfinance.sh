79f8964a73bb093954985b0c25922a57230e1e7e
Krystian Safjan <ksafjan@gmail.com>
2021-12-11 04:40:33 +0100

    Updated finance script

--- runonce-scripts/ron-501-yfinance.sh
+++ runonce-scripts/ron-501-yfinance.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -d ~/.virtualenvs/yfinance/bin/activate ]]; then
-	source ~/.virtualenvs/yfinance/bin/activate	
+# if [[ -d ~/.virtualenvs/yfinance/bin/activate ]]; then
+# 	source ~/.virtualenvs/yfinance/bin/activate	
 
-	# run python script here
-	echo "> yfinance advanced analysis will go here <"
-	deactivate
-else
-	echo "missing yfinance venv"
-fi
+# 	# run python script here
+# 	echo "> yfinance advanced analysis will go here <"
+# 	deactivate
+# else
+# 	echo "missing yfinance venv"
+# fi
