2ef2a8cfa0c977b3117510bbf209c01e7481b804
Krystian Safjan <ksafjan@gmail.com>
2022-05-09 21:02:40 +0200

    Fixed list file name

--- restore/013-install-often-used-packages.sh
+++ restore/013-install-often-used-packages.sh
@@ -3,6 +3,6 @@
 #
 # often used packages needed for convenient work
 
-./pkg-from-the-list.sh ./pkg/020_ext_often_used.txt
+./pkg-from-the-list.sh ./pkg/020_often_used.txt
 
 echo "### Often used packages installed"
