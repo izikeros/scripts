a36ee31021bf80c4f4fa6b2bc875d50af17c27dc
Krystian Safjan <ksafjan@gmail.com>
2022-05-10 09:39:39 +0200

    Fixed docker polybar script

--- polybar-scripts/docker-dir-size.sh
+++ polybar-scripts/docker-dir-size.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-docker_dir=$(sudo du -sh /var/lib/docker | awk '{print $1}')
-echo "D: $docker_dir"
+
+if [ -d /var/lib/docker ]; then
+    docker_dir=$(sudo du -sh /var/lib/docker | awk '{print $1}')
+    echo "D: $docker_dir"
+fi
