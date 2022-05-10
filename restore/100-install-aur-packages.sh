6dfae6a82039cfcd575b8af3d31f9384340b431f
Krystian Safjan <ksafjan@gmail.com>
2022-05-10 09:40:16 +0200

    Fixed restore scripts

--- restore/100-install-aur-packages.sh
+++ restore/100-install-aur-packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-./pkg-yay-from-the-list.sh ./pkg/aur.txt
+./pkg-yay-from-the-list.sh ./pkg/130_ext_long_list_manual.txt
 
 echo "### AUR packages installed"
--- restore/130-merge-xfce40-keyboard-shortcuts.sh
+++ restore/130-merge-xfce40-keyboard-shortcuts.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-meld ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ~/dotfiles/.config/xfce4/xfwm4/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+meld ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ~/dotfiles/dotfiles/.config/xfce4/xfwm4/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
