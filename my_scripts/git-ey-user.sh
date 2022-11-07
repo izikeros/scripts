783560e5b36b96d570a5ba6d71683bbc48ab4ddc
Krystian Safjan <ksafjan@gmail.com>
2022-11-07 16:47:31 +0100

    Update EY git user - add signing commits

--- my_scripts/git-ey-user.sh
+++ my_scripts/git-ey-user.sh
@@ -6,3 +6,9 @@ set -e
 
 git config user.email "krystian.safjan@gds.ey.com"
 git config user.name "Krystian Safjan"
+
+git config gpg.format ssh
+git config gpg.ssh.allowedSignersFile ~/.config/git/allowed_ssh_signers
+git config user.signingKey ~/.ssh/ey_azure_devops_rsa.pub
+git config commit.sign true
+
