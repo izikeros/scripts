02d6d1f7b90262ef8c169957df3797454675b263
Krystian Safjan <ksafjan@gmail.com>
2022-11-07 16:46:13 +0100

    Add codebase classes script
    List classes in the software project

---
+++ my_scripts/codebase-classes.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo
+echo "Classes:"
+echo "========"
+find ./ -name "*.py" -print0 | xargs -0 grep -h "^[[:blank:]]*class .*:" | sed 's/://g' | sed 's/^[[:space:]]*//' | sed 's/^class//g'
