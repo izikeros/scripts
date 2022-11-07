0206569c19d88becbecfeb52fb9d94a0e9b20242
Krystian Safjan <ksafjan@gmail.com>
2022-11-07 16:47:05 +0100

    Add script to display info about project size
    Codebase size (lines), num classes, num functions

---
+++ my_scripts/codebase-how-big.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Codebase size:"
+echo "=============="
+pygount --format=summary . | grep -E --color=never "Python|Language"
+
+echo
+echo "Number of classes:"
+echo "=================="
+find ./ -name "*.py" -print0 | xargs -0 grep -ch "^[[:blank:]]*class.*:" | awk -F":" '{print $(NF)}' | awk '{sum+=$1;} END{print sum;}'
+
+echo
+echo "Number of functions:"
+echo "===================="
+find ./ -name "*.py" -print0 | xargs -0 grep -ch "^[[:blank:]]*def.*:" | mawk -F":" '{print $(NF)}' | awk '{sum+=$1;} END{print sum;}'
