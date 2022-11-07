67ac6b8b74b76620f816a1dc15efe44c63f03f58
Krystian Safjan <ksafjan@gmail.com>
2022-11-07 16:45:30 +0100

    Update outliner script

--- my_scripts/outliner.py
+++ my_scripts/outliner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+# import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -8,9 +9,11 @@ if __name__ == "__main__":
     try:
         url = sys.argv[1]
     except IndexError:
-        print("Usage: python3 scrapper.py URL")
+        print("Usage: python3 outliner.py URL")
         sys.exit(1)
 
+    # read html from url using requests and pass it to BeautifulSoup to run html parser
+    #html = requests.get(url).text
     html = urlopen(url)
     bs = BeautifulSoup(html, "html.parser")
     titles = bs.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])
@@ -18,6 +21,5 @@ if __name__ == "__main__":
     # iterate over the titles and print them with indentation:
     # h1 with no indentation, h2 with 1 tab, h3 with 2 tabs, etc.
     for title in titles:
-        txt = title.get_text().strip()
-        if txt:
+        if txt := title.get_text().strip():
             print("\t" * (int(title.name[1]) - 1) + txt)
