444fb16a4c1948cd19d118db8d068664fdcda066
Krystian Safjan <ksafjan@gmail.com>
2022-11-30 06:14:20 +0100

    updated mirror-rank script

--- restore/180-rank-mirrors.sh
+++ restore/180-rank-mirrors.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo reflector --verbose --country 'Poland' -l 5 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose --country 'Poland,Germany,Czechia' -l 10 --sort rate --save /etc/pacman.d/mirrorlist
