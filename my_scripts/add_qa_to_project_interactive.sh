2831fa99e401762bb79bbd40c5091ed483d74e21
Krystian Safjan <ksafjan@gmail.com>
2023-12-29 05:03:52 +0100

    propose most used options at the top of the list

--- my_scripts/add_qa_to_project_interactive.sh
+++ my_scripts/add_qa_to_project_interactive.sh
@@ -31,15 +31,14 @@ fi
 
 # download required files
 download_file https://gist.github.com/izikeros/937b05e81b5dca81d3daf309ea6bad20/raw ./.pre-commit-config.yaml
-download_file https://gist.github.com/izikeros/4c38eb8ee022eda256a7880ddbd6f834/raw ./.bandit
-download_file https://gist.github.com/izikeros/946d65c15190b7cf48590f629119ab60/raw ./mypy.ini
 download_file https://gist.github.com/izikeros/c938c49f5ad38dcc74a5671e85f694e7/raw ./.flake8
-download_file https://gist.github.com/izikeros/1a2243e083d083c2a4f97cf9c19648d9/raw ./tox.ini
-download_file https://gist.github.com/izikeros/4c79e2f82b68083b8a0b23badc36204d/raw ./pytest.ini
-download_file https://gist.github.com/izikeros/beac1ca8de132ad527ab27e32a950a11/raw ./requirements-dev.txt
 download_file https://gist.github.com/izikeros/87b544d1ba644cc89dccca04fc87d65d/raw ./README.md
 download_file https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore ./.gitignore
 echo "(MIT license)"
 download_file https://raw.githubusercontent.com/licenses/license-templates/master/templates/mit.txt LICENSE
 download_file https://gist.githubusercontent.com/izikeros/d4359ff65052fc21df778a7f7373f0e2 .editorconfig
-
+download_file https://gist.github.com/izikeros/4c38eb8ee022eda256a7880ddbd6f834/raw ./.bandit
+download_file https://gist.github.com/izikeros/946d65c15190b7cf48590f629119ab60/raw ./mypy.ini
+download_file https://gist.github.com/izikeros/1a2243e083d083c2a4f97cf9c19648d9/raw ./tox.ini
+download_file https://gist.github.com/izikeros/4c79e2f82b68083b8a0b23badc36204d/raw ./pytest.ini
+# download_file https://gist.github.com/izikeros/beac1ca8de132ad527ab27e32a950a11/raw ./requirements-dev.txt
