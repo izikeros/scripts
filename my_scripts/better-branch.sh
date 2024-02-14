c933bd4e73c97cd4d277fa628ef9431559af5b07
Krystian Safjan <ksafjan@gmail.com>
2024-02-14 08:42:35 +0100

    add displaying branches with rich information
    inspired by: https://youtu.be/aolI_Rz0ZqY?si=3nbEf8m2sKZDJRQU&t=468

---
+++ my_scripts/better-branch.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# inspiration from https://youtu.be/aolI_Rz0ZqY?si=3nbEf8m2sKZDJRQU&t=468
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+branches=$(git for-each-ref --sort=committerdate refs/heads --format='%(refname:short)')
+# revert the list of branches
+branches=$(echo "$branches" | tac)
+
+# Print header
+printf "%-5s %-5s %-30s %-25s %-35s\n" "Ahead" "Behind" "Branch" "Last Commit" "Description"
+printf '%.0s-' {1..80}; echo ""  # Print dash line
+
+
+# Iterate over each branch
+# $(git branch --format='%(refname:short)')
+for branch in $branches; do
+  #  commit_date=$(git show -s --format=%ci $branch | cut -d' ' -f1) # Extract the date part
+  commit_age=$(git log -1 --pretty=format:"%ar" $branch) # Get the commit age
+  #  commit_subject=$(git show -s --format=%s $branch) # Get the last commit's description
+  ahead=$(git rev-list --left-only --count $current_branch...$branch)
+  behind=$(git rev-list --right-only --count $current_branch...$branch)
+  desc=$(git config branch.$branch.description)
+
+#  # Highlight current branch in green
+#  if [ $branch == $current_branch ]; then
+#    branch="* \033[0;32m$branch\033[0m"
+#  else
+#    branch="  $branch"
+#  fi
+  # Print branch details
+  printf "%-5s %-5s %-30s %-25s %-35s\n" $ahead $behind $branch "$commit_age" "$desc"
+done
+# reset color
+#echo -e "\033[0m"
+
+
+# #!/bin/bash
+
+# # Ordering branches by last commit time ascending
+# branches=$(git for-each-ref --sort=committerdate refs/heads --format='%(refname:short)')
+
+# current=$(git rev-parse --abbrev-ref HEAD)
+
+# for branch in $branches; do
+#   desc=$(git config branch.$branch.description)
+#   ahead=$(git rev-list --left-only --count $current...$branch)
+#   behind=$(git rev-list --right-only --count $current...$branch)
+#   commit_age=$(git log -1 --pretty=format:"%ar" $branch)
+#   # commit_subject=$(git show -s --format=%s $branch) # Get the last commit's description
+
+#   # Highlight current branch in green
+#   if [ $branch == $current ]; then
+#     branch="* \033[0;32m$branch\033[0m"
+#   else
+#     branch="  $branch"
+#   fi
+
+#   # Print branch details
+#   printf "%-5s %-5s %-40s %-15s %-15s\n" $ahead $behind $branch "$commit_age" "$desc"
+# done
+
+# # reset color
+# echo -e "\033[0m"
