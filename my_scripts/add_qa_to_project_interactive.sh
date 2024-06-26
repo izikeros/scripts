4411e0b95f85105123e9a9ee3d565bd5e64e65ef
Krystian Safjan <ksafjan@gmail.com>
2024-06-26 15:26:43 +0200

    add git-cliff config

--- my_scripts/add_qa_to_project_interactive.sh
+++ my_scripts/add_qa_to_project_interactive.sh
@@ -13,13 +13,17 @@ function download_file() {
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         curl -sL $url -o $filename
     elif [[ $REPLY =~ ^[Qq]$ ]]; then
-        read -p "Are you sure you want to exit? [y/n] " -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            exit 0
-        fi
+        # read -p "Are you sure you want to exit? [y/n] " -n 1 -r
+        # echo
+        # if [[ $REPLY =~ ^[Yy]$ ]]; then
+        #     exit 0
+        # fi
+        exit 0
     fi
 }
+
+# most frequently used
+download_file https://gist.github.com/izikeros/8cbc53d9fd6c40b38b52bebd718fce37/raw ./ruff.toml
 
 # pre-commit install
 read -p "Do you want to install pre-commit package and autoupdate? " -n 1 -r
@@ -36,9 +40,10 @@ download_file https://gist.github.com/izikeros/87b544d1ba644cc89dccca04fc87d65d/
 download_file https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore ./.gitignore
 echo "(MIT license)"
 download_file https://raw.githubusercontent.com/licenses/license-templates/master/templates/mit.txt LICENSE
-download_file https://gist.githubusercontent.com/izikeros/d4359ff65052fc21df778a7f7373f0e2 .editorconfig
+download_file https://gist.github.com/izikeros/d4359ff65052fc21df778a7f7373f0e2 .editorconfig
 download_file https://gist.github.com/izikeros/4c38eb8ee022eda256a7880ddbd6f834/raw ./.bandit
 download_file https://gist.github.com/izikeros/946d65c15190b7cf48590f629119ab60/raw ./mypy.ini
 download_file https://gist.github.com/izikeros/1a2243e083d083c2a4f97cf9c19648d9/raw ./tox.ini
 download_file https://gist.github.com/izikeros/4c79e2f82b68083b8a0b23badc36204d/raw ./pytest.ini
+download_file https://gist.github.com/izikeros/b150d96360ee7bda5e0c7a750e90561d/raw/ ./cliff.toml
 # download_file https://gist.github.com/izikeros/beac1ca8de132ad527ab27e32a950a11/raw ./requirements-dev.txt
