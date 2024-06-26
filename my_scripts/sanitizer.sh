4944c364eeda1487035cfcde4827a2b77803bc45
Krystian Safjan <ksafjan@gmail.com>
2024-06-26 15:27:38 +0200

    add cleaner output from sanitizer.sh

--- my_scripts/sanitizer.sh
+++ my_scripts/sanitizer.sh
@@ -2,9 +2,15 @@
 
 
 FILE_TYPE=${1:-*}
-echo "F:$FILE_TYPE"
+# display the file type only if it is not *
+[ "$FILE_TYPE" != "*" ] && echo "F:$FILE_TYPE"
 find . -iname "$FILE_TYPE" | while read f
 do
+  # avoid having error message:
+  # mv: . and ./. are identical
+  if [ "$f" = "." ] || [ "$f" = "./." ]; then
+    continue
+  fi
 	echo "$f"
 	# allowed: letters, numbers and dot, coma and slash. The rest would be replaced by underscore
 	nf=$(echo "$f" | sed -e 's/[^A-Za-z0-9/._-]/_/g')
