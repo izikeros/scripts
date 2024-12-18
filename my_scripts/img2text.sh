ad7e64e4997673ca917892bab28bd3b5f888f085
Krystian Safjan <ksafjan@gmail.com>
2024-12-18 10:52:11 +0100

    add ocr automation script

---
+++ my_scripts/img2text.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title img2text
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Developer Utils
+
+# Documentation:
+# @raycast.description Take screenshoot and run OCR, copy text to clipboard
+# @raycast.author Krystian Safjan
+# @raycast.authorURL https://safjan.com
+
+# based on source: https://winsmarts.com/clipboard-image-to-text-via-ocr-a65d4e7f6482
+
+#set -o xtrace
+#set -e
+#OUTPUT_TXT_FILE="/tmp/output.txt"
+#OUTPUT_IMG_FILE="/tmp/scr_img.png"
+#
+## ensure the file is not there (delete it)
+[ -f "/tmp/output.txt" ] && rm "/tmp/output.txt"
+[ -f "/tmp/scr_img.png" ] && rm "/tmp/scr_img.png"
+
+# do the ocr and redirect errors to /dev/null
+current_dir=$(pwd)
+cd /tmp || exit
+
+screencapture -i scr_img.png
+#echo "..Captured"
+tesseract "scr_img.png" /tmp/output 2> /dev/null
+#echo "..OCR done"
+#echo "------"
+#cat /tmp/output.txt
+#echo "------"
+pbcopy < /tmp/output.txt
+#echo "..Copied to clipboard"
+# display the text - how many characters were copied
+#cat /tmp/output.txt | wc -m
+
+# Clean-up: delete the files
+[ -f "/tmp/output.txt" ] && rm "/tmp/output.txt"
+[ -f "/tmp/scr_img.png" ] && rm "/tmp/scr_img.png"
+#echo "..Cleaned. Done"
+cd "$current_dir" || exit
+
+
