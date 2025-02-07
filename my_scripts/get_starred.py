51c92940334a9442787614b85f2c9fe43093f662
Krystian Safjan <ksafjan@gmail.com>
2025-02-07 05:07:39 +0100

    feat: add uv shebang

--- my_scripts/get_starred.py
+++ my_scripts/get_starred.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+#     "requests",
+# ]
+# ///
 """download GitHub starred repos"""
 import json
 
